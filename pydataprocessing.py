65dfac46a4323e79f639408a526a635686da9296
handofkwll <jfl@roe.ac.uk>
2015-11-17 12:03:05 +0000

    Added data_quality parameter and writes out dirty cube to FITS file.

--- pydataprocessing.py
+++ pydataprocessing.py
@@ -3,20 +3,26 @@ from __future__ import absolute_import
 import astropy.io.fits as pyfits
 import collections
 import datetime
+import os.path
 import pp
 
+import common.commonobjects as co
 import cleanimage
 import dirtyimage
 import reduceinterferogram
 import renderer
 import readfits
+import writefits
 
 
 class PyDataProcessing(object):
     """Data processing module to reduce data from FISICA simulator.
     """
-    def __init__(self, fitsfile):
+    def __init__(self, fitsfile, data_quality='noisy'):
         self.fitsfile = fitsfile
+        if data_quality not in ['noisy', 'pure']:
+            raise Exception, 'bad data_quality: %s' % data_quality
+        self.data_quality = data_quality
 
         self.result = collections.OrderedDict()
 
@@ -38,17 +44,33 @@ class PyDataProcessing(object):
 
         # recover spectra from interferograms
         reduceint = reduceinterferogram.ReduceInterferogram(
-          previous_results=self.result,
+          previous_results=self.result, data_quality=self.data_quality,
           job_server=self.job_server)
         self.result['reduceinterferogram'] = reduceint.run()
         print reduceint
    
         # construct dirty image
-        dirty = dirtyimage.DirtyImage(
-          previous_results=self.result,
+        dirty = dirtyimage.DirtyImage(previous_results=self.result,
           job_server=self.job_server)
         self.result['dirtyimage'] = dirty.run()
         print dirty
+
+        # write the cube to FITS
+        dirty_image = self.result['dirtyimage']['dirtyimage']
+        spatial_axis = self.result['dirtyimage']['spatial axis [arcsec]']
+        wn_axis = self.result['dirtyimage']['wavenumber [cm-1]']
+
+        axis1 = co.Axis(data=spatial_axis, title='RA', units='arcsec')
+        axis2 = co.Axis(data=spatial_axis, title='Dec', units='arcsec')
+        axis3 = co.Axis(data=wn_axis, title='Frequency', units='cm-1')
+        cube = co.Image(data=dirty_image, axes=[axis1, axis2, axis3])
+
+        dirtyfitsfile = os.path.splitext(self.fitsfile)[0]
+        dirtyfitsfile = '%s.%s.dirtycube.fits' % (dirtyfitsfile,
+          self.data_quality)
+
+        cubewriter = writefits.WriteFITSCube(cube, dirtyfitsfile)
+        cubewriter.run()
    
         # construct clean image
 #    #    clean = cleanimage.CleanImage(
